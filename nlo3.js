147f9fb04631a1fbb4dfbe585d4ede46c69d8764
Mihail292 <58395790+Mihail292@users.noreply.github.com>
2020-11-15 21:07:24 +0630

    t

--- nlo3.js
+++ nlo3.js
@@ -72,16 +72,20 @@ const shaderMaterial = new THREE.ShaderMaterial({
     uniforms: {
         // . . .
     },
-    vertexShader:   document.getElementById('sphere-vertex-shader').textContent,
-    fragmentShader: document.getElementById('sphere-fragment-shader').textContent
+  //  vertexShader:   document.getElementById('sphere-vertex-shader').textContent,
+  //  fragmentShader: document.getElementById('sphere-fragment-shader').textContent
 });
 
 
-var plane,plane2;
+var plane,plane2;var material2,material3;
 function meshes(){
 var geometry = new THREE.SphereBufferGeometry( 4, 64, 64 );
 var material = new THREE.MeshPhongMaterial();
-var material2 = new THREE.MeshPhongMaterial({color:'red'});
+material2 = new THREE.MeshPhongMaterial({color:'red'});
+material3 = new THREE.MeshPhongMaterial({color:'yellow'});
+material3.emissive.setRGB(0.46,0.6,0.77);
+
+material3.emissiveIntensity=1.5;
 plane = new THREE.Mesh( geometry, material );
 scene.add( plane );
 
@@ -91,10 +95,12 @@ plane2 = new THREE.Mesh( geometry2, material2 );
 scene.add( plane2 );
 //plane.position.z=-10;plane.position.y=5;
 lights= new THREE.SpotLight();
-lights.position.z=-15;
-lights.color.setRGB(0,6,2);
-
-scene.add(lights);
+lights.position.z=20;
+lights.color.setRGB(0,0.612,0.57);
+lights2= new THREE.SpotLight();
+lights2.position.z=-20;
+lights2.color.setRGB(0.07,0.26,0.67);
+scene.add(lights,lights2);
 camera.position.z=-70;
 controls.target=plane.position;
 
@@ -108,36 +114,72 @@ plane2.material.emissiveIntensity=1;plane2.material.emissive.setRGB(0.46,0.6,0.7
 
 }
 
+const curve1 = new THREE.SplineCurve( [
+new THREE.Vector2( 10, 0 ),
+new THREE.Vector2( 5, 5 ),
+new THREE.Vector2( 10, 10 ),
+ new THREE.Vector2( 20, -10 ),
+ new THREE.Vector2( -0, -10 ),
+  new THREE.Vector2( -10, -5 ),
+  new THREE.Vector2( -20, -10 ),
+  new THREE.Vector2( -15, 10 ),
+  new THREE.Vector2( -10, 0 ),
+	new THREE.Vector2( -5, 5 ),
+	new THREE.Vector2( 0, 0 ),
+	new THREE.Vector2( 5, -5 ),
+	new THREE.Vector2( 10, 0 )
+] );
+
 const curve = new THREE.EllipseCurve(
 	0,  0,            // ax, aY
 	5, 5,           // xRadius, yRadius
-	Math.PI/2, 2* Math.PI/2, // aStartAngle, aEndAngle
+	-Math.PI/6-Math.PI/6, Math.PI/2, // aStartAngle, aEndAngle
 	false,            // aClockwise
 	0                 // aRotation
 );
-
             var points = curve.getPoints( 50 );
-
+var pointsCurve = curve1.getPoints( 1000 );
             var geometry = new THREE.Geometry().setFromPoints( points );
-
+var geometryCurve1 = new THREE.Geometry().setFromPoints( pointsCurve );
             var material = new THREE.LineBasicMaterial( { color : 0xff0000 } );
 
             // Create the final object to add to the scene
             curveObject = new THREE.Line( geometry, material );
+            curveObject1 = new THREE.Line( geometryCurve1 , material );
 
-            //scene.add( curveObject );
+            //scene.add( curveObject1 );
 
-            curveObject.curve = curve;
+            curveObject.curve = curve;curveObject1.curve = curve1;
 				plane2.position.set(curveObject.geometry.vertices[0].x,curveObject.geometry.vertices[0].y,curveObject.geometry.vertices[0].z);
-var t=0;
-var tuda=true;
-var suda=false;
-  function animate() {
-    requestAnimationFrame( animate);
+        var ufo;
+        var loader = new THREE.GLTFLoader();
+         loader.load( '/nlo.glb', function ( load ) {
+           ufo=load;
+           ufo.scene.getObjectByName('Сфера').material=material3;
+           ufo.scene.getObjectByName('Икосфера').material=material3;
+           ufo.scene.getObjectByName('Torus001').material=material3;
+           ufo.scene.getObjectByName('Torus002').material=material3;
+           ufo.scene.getObjectByName('Torus003').material=material3;
+           ufo.scene.getObjectByName('Torus004').material=material3;
+           ufo.scene.getObjectByName('Сфера003').material= new THREE.MeshLambertMaterial({});
+           ufo.scene.getObjectByName('Сфера003').material.color.setRGB(0.95,0.98,0);
+           //ufo.scene.scale.set(1.5,1.5,1.5);
+           scene.add(ufo.scene);
+         });
 
-  if(tuda){plane2.position.set(curveObject.geometry.vertices[t].x,curveObject.geometry.vertices[t].y,curveObject.geometry.vertices[t].z);t++;if(t==50){tuda=false;suda=true}};
-  if(suda){plane2.position.set(curveObject.geometry.vertices[t].x,curveObject.geometry.vertices[t].y,curveObject.geometry.vertices[t].z);t--;if(t==0){suda=false;tuda=true}}
 
+
+
+        var t=1000;
+        var tt=50;
+        var tuda=true;
+        var suda=false;
+          function animate() {
+            requestAnimationFrame( animate);
+if(ufo){
+  ufo.scene.position.set(curveObject1.geometry.vertices[t].x,curveObject1.geometry.vertices[t].y,curveObject1.geometry.vertices[t].z);t--;if(t==0){t=1000}}
+         if(tuda){plane2.position.set(curveObject.geometry.vertices[tt].x,curveObject.geometry.vertices[tt].y,curveObject.geometry.vertices[tt].z);tt--;if(tt==0){tuda=false;suda=true}};
+         if(suda){plane2.position.set(curveObject.geometry.vertices[tt].x,curveObject.geometry.vertices[tt].y,curveObject.geometry.vertices[tt].z);tt++;if(tt==50){suda=false;tuda=true}}
     composer.render(scene, camera);
     //const composer = new EffectComposer( renderer );
 

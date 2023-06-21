5ca0710997d0699862696cdb2ec36eb4eb5305ff
Mihail292 <58395790+Mihail292@users.noreply.github.com>
2023-06-21 18:30:00 +0300

    Create msbase.js

---
+++ msbase.js
@@ -0,0 +1,13 @@
+import {initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-SERVICE.js'
+var firebaseConfig = {
+  apiKey: "AIzaSyCB7-zaSIttCWPdhOEOjn83RD79thKULR4",
+  authDomain: "houndredone-63426.firebaseapp.com",
+  databaseURL: "https://houndredone-63426.firebaseio.com",
+  projectId: "houndredone-63426",
+  storageBucket: "houndredone-63426.appspot.com",
+  messagingSenderId: "955812981428",
+  appId: "1:955812981428:web:a748e9d46f3dd88d4cc5e1",
+  measurementId: "G-J1G01LTK1Q"
+};
+
+const app = initializeApp(firebaseConfig);

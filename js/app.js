760e04f4125e8260704416eb6a7534a508d6dfa9
Mihail292 <58395790+Mihail292@users.noreply.github.com>
2024-10-09 17:18:36 +0300

    mnb

--- js/app.js
+++ js/app.js
@@ -145,9 +145,31 @@ var APP = {
 		}
 
 		var time, startTime, prevTime;
-
+		
+		const raycaster = new THREE.Raycaster();
+		const pointer = new THREE.Vector2();
+		
+		function onPointerMove( event ) {
+		
+			// calculate pointer position in normalized device coordinates
+			// (-1 to +1) for both components
+		
+			pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+			pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+		
+		}
 		function animate() {
 
+			raycaster.setFromCamera( pointer, camera );
+
+			// calculate objects intersecting the picking ray
+			const intersects = raycaster.intersectObjects( scene.children );
+		
+			for ( let i = 0; i < intersects.length; i ++ ) {
+		
+				intersects[ i ].object.material.color.set( 0xff0000 );
+		
+			}
 			time = performance.now();
 
 			try {
